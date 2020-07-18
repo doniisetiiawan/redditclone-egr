67771b4d95cba4836826a15cea92a75a00be473f
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-18 07:39:25 +0700

    ...

--- src/containers/Posts/index.jsx
+++ src/containers/Posts/index.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 class Posts extends Component {
   render() {
+    const { posts } = this.props;
+
     if (this.props.loading) {
       return <div>Loading...</div>;
     }
 
     return (
       <div className="Posts">
-        {Object.keys(this.props.posts).map((key) => (
-          <div key={key}>{this.props.posts[key].title}</div>
+        {Object.keys(posts).map((key) => (
+          <div key={key}>{posts[key].title}</div>
         ))}
       </div>
     );
